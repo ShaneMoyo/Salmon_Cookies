616f62f083844e0dbd62ef5143dad1f8e4651476
Shane Moyo <smoyo@pdx.edu>
2017-08-08 07:31:48 -0700

    added js file

---
+++ app.js
@@ -0,0 +1,5 @@
+var pdxAirport = {minCutomers:'23', maxCustomers:'65', avgCookie:'6.3'};
+var pioneerSquare = {minCutomers:'3', maxCustomers:'24', avgCookie:'1.2'};
+var powells = {minCutomers:'11', maxCustomers:'38', avgCookie:'3.7'};
+var stJohns = {minCutomers:'20', maxCustomers:'38', avgCookie:'2.3'};
+var waterfront = {minCutomers:'2', maxCustomers:'16', avgCookie:'4.6'};
