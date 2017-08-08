a0c94a4faa91b5403f149b02cae3b9654a49c748
Shane Moyo <smoyo@pdx.edu>
2017-08-08 11:48:47 -0700

    corrected add to dom function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var hours = ['8:00 a.m.', '9:00 a.m.', '10:00 a.m.', '11:00 a.m.','12:00 p.m.', 
-    '1:00 p.m.', '2:00 p.m.','3:00 p.m.', '4:00 p.m.', '5:00 p.m.', '6:00 p.m.']
+var hours = ['6:00 a.m.', '7:00 a.m.', '8:00 a.m.', '9:00 a.m.', '10:00 a.m.', '11:00 a.m.','12:00 p.m.', 
+    '1:00 p.m.', '2:00 p.m.','3:00 p.m.', '4:00 p.m.', '5:00 p.m.', '6:00 p.m.', '7:00 p.m.' , '8:00 p.m.']
 
 var pdxAirport = {
     min: 23,
@@ -30,81 +30,188 @@ var pdxAirport = {
     newList.innerHTML = (hours[i] + ': ' + this.cookiesPerHourArray[i] )
     list.appendChild(newList);
     }
+    var total = 0;
+    for( i = 0; i < 15; i++ ) {
+        total += this.cookiesPerHourArray[i];
+
+    }
+    var list = document.getElementById('pdxcookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = ('Total: ' + total )
+    list.appendChild(newList);
     },
 
     };
 
-
-var pioneerSquare = {
-    minCutomers: 3,
-    maxCustomers: 24,
-    avgCookie: 1.2,
-    cookiesByHour: [],
+    var pioneerSquare = {
+    min: 23,
+    max: 65,
+    avgCookie: 6.3,
+    cookiesPerHourArray: [],
 
     custPerHour: function(){
-       return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+      return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
     },
 
     cookiesPerHour: function(){
        return Math.floor(this.custPerHour() * this.avgCookie);
     },
     
-
-    };
-
-var powells = {
-    minCutomers: 11,
-    maxCustomers: 38,
-    avgCookie: 3.7,
-
-    custPerHour: function(){
-       return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    },
-
-    cookiesPerHour: function(){
-       return Math.floor(this.custPerHour() * this.avgCookie);
-    },
-    };
-
-var stJohns = {
-    minCutomers: 20,
-    maxCustomers: 38,
-    avgCookie: 2.3,
-
-    custPerHour: function(){
-       return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    },
-
-    cookiesPerHour: function(){
-       return Math.floor(this.custPerHour() * this.avgCookie);
-    },
-    };
-
-var waterfront = {
-    minCutomers: 2,
-    maxCustomers: 16,
-    avgCookie: 4.6,
-
-    custPerHour: function(){
-       return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
-    },
-
-    cookiesPerHour: function(){
-       return Math.floor(this.custPerHour() * this.avgCookie);
+    calcCookiesPerHour: function(){
+        for(i = 0; i < 15; i++ ){
+            this.cookiesPerHourArray.push(this.cookiesPerHour());
+        }
+        return this.cookiesPerHourArray
     },
     
-    };
-
-    
-    function addToDom(){
+    addToDom: function(){
         this.calcCookiesPerHour()
         for( i = 0; i < 15; i++ ) {
-    var list = document.getElementById('pdxcookie')
+    var list = document.getElementById('pioneercookie')
     var newList = document.createElement('li');
     newList.innerHTML = (hours[i] + ': ' + this.cookiesPerHourArray[i] )
     list.appendChild(newList);
     }
-    }
+    var total = 0;
+    for( i = 0; i < 15; i++ ) {
+        total += this.cookiesPerHourArray[i];
 
+    }
+    var list = document.getElementById('pioneercookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = ('Total: ' + total )
+    list.appendChild(newList);
+    },
+
+    };
+var powells = {
+    min: 23,
+    max: 65,
+    avgCookie: 6.3,
+    cookiesPerHourArray: [],
+
+    custPerHour: function(){
+      return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    },
+
+    cookiesPerHour: function(){
+       return Math.floor(this.custPerHour() * this.avgCookie);
+    },
+    
+    calcCookiesPerHour: function(){
+        for(i = 0; i < 15; i++ ){
+            this.cookiesPerHourArray.push(this.cookiesPerHour());
+        }
+        return this.cookiesPerHourArray
+    },
+    
+    addToDom: function(){
+        this.calcCookiesPerHour()
+        for( i = 0; i < 15; i++ ) {
+    var list = document.getElementById('powellscookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = (hours[i] + ': ' + this.cookiesPerHourArray[i] )
+    list.appendChild(newList);
+    }
+    var total = 0;
+    for( i = 0; i < 15; i++ ) {
+        total += this.cookiesPerHourArray[i];
+
+    }
+    var list = document.getElementById('powellscookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = ('Total: ' + total )
+    list.appendChild(newList);
+    },
+
+    };
+var stJohns = {
+    min: 23,
+    max: 65,
+    avgCookie: 6.3,
+    cookiesPerHourArray: [],
+
+    custPerHour: function(){
+      return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    },
+
+    cookiesPerHour: function(){
+       return Math.floor(this.custPerHour() * this.avgCookie);
+    },
+    
+    calcCookiesPerHour: function(){
+        for(i = 0; i < 15; i++ ){
+            this.cookiesPerHourArray.push(this.cookiesPerHour());
+        }
+        return this.cookiesPerHourArray
+    },
+    
+    addToDom: function(){
+        this.calcCookiesPerHour()
+        for( i = 0; i < 15; i++ ) {
+    var list = document.getElementById('stJohnscookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = (hours[i] + ': ' + this.cookiesPerHourArray[i] )
+    list.appendChild(newList);
+    }
+    var total = 0;
+    for( i = 0; i < 15; i++ ) {
+        total += this.cookiesPerHourArray[i];
+
+    }
+    var list = document.getElementById('stJohnscookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = ('Total: ' + total )
+    list.appendChild(newList);
+    },
+
+    };
+    
+var waterFront = {
+    min: 23,
+    max: 65,
+    avgCookie: 6.3,
+    cookiesPerHourArray: [],
+
+    custPerHour: function(){
+      return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    },
+
+    cookiesPerHour: function(){
+       return Math.floor(this.custPerHour() * this.avgCookie);
+    },
+    
+    calcCookiesPerHour: function(){
+        for(i = 0; i < 15; i++ ){
+            this.cookiesPerHourArray.push(this.cookiesPerHour());
+        }
+        return this.cookiesPerHourArray
+    },
+    
+    addToDom: function(){
+        this.calcCookiesPerHour()
+        for( i = 0; i < 15; i++ ) {
+    var list = document.getElementById('waterfrontcookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = (hours[i] + ': ' + this.cookiesPerHourArray[i] )
+    list.appendChild(newList);
+    }
+    var total = 0;
+    for( i = 0; i < 15; i++ ) {
+        total += this.cookiesPerHourArray[i];
+
+    }
+    var list = document.getElementById('waterfrontcookie')
+    var newList = document.createElement('li');
+    newList.innerHTML = ('Total: ' + total )
+    list.appendChild(newList);
+    },
+
+    };
+    
     pdxAirport.addToDom();
-    console.log(pdxAirport.calcCookiesPerHour());
+    pioneerSquare.addToDom();
+    powells.addToDom();
+    stJohns.addToDom();
+    waterFront.addToDom();
+    
